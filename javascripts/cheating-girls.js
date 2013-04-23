3db04b4e22206e2d704178ba1487aaf241193b73
Kimmy Leo <kenpusney@gmail.com>
2013-04-24 00:18:15 +0800

    Update cheating-girls.js

--- javascripts/cheating-girls.js
+++ javascripts/cheating-girls.js
@@ -1,3 +1,14 @@
 $(function(){
-  console.log("Hello,world!");
+  if(location.href.indexOf("#") !== -1){
+		trans = location.href.split("#")[1];
+		$.ajax({
+			url   : "http://127.0.0.1:9511/cheating-girls",
+			type  : "GET",
+			data  : trans,
+			success : function(){
+				console.log("success!");
+				window.close();
+			}
+		})
+	}
 })
