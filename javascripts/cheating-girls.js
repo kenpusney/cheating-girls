698a8aee3ec6e17dc1e41f44a064e1d28687bb20
Kimmy Leo <kenpusney@gmail.com>
2013-05-05 00:37:21 +0800

    day 2 prettify

--- javascripts/cheating-girls.js
+++ javascripts/cheating-girls.js
@@ -9,31 +9,32 @@ function login(){
             localStorage.setItem("name",l.name);
             localStorage.setItem("access_token",l.access_token);
             localStorage.setItem("openid",l.openid);
-            $("#hellomsg").text("Hello, "+localStorage.getItem('nick'));
+            $("#hellomsg").text("你好，"+localStorage.getItem('nick'));
             share();
             getgirlcount();
         },
         function(e){
-            alert("Oh sorry, login failed, try again.")
+            alert("抱歉，登录失败，请重试。\nOh sorry, login failed, try again.")
         }
         );
 }
 
 function share(){
-    T.api("/t/add",{content:"第二天，人们缺少发现美的工具，于是，人们需要帮助。 http://kenpusney.github.io/cheating-girls/cheating-girls.html",
+    T.api("/t/add",{content:"第二天，人们缺少发现美的工具，于是，人们需要帮助。 追随我们去寻找美丽吧： http://kenpusney.github.io/cheating-girls/cheating-girls.html",
                     clientip:"127.0.0.1"},
                     "json",
                     "post")
     .success(function(r){
-        alert("Welcome "+localStorage.getItem("nick")+".");
+        alert("欢迎，"+localStorage.getItem("nick")+"！");
     })
     .error(function(code,msg){
-        alert("Error.");
+        alert("呀，出错啦！要不要再来一次？\nOops, we get an error, try again?");
     });
 }
 
 function logout(){
     T.logout();
+    $("#hellomsg").text("你好，陌生人");
     localStorage.clear();
 }
 
